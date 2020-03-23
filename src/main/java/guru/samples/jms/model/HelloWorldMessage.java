da07f5c60e45fc4be7fe13e7dbc2f1b06eb208f0
digid0c <digidoc22@gmail.com>
2020-03-24 00:19:59 +0200

    Create simple message POJO

---
+++ src/main/java/guru/samples/jms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package guru.samples.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    private static final long serialVersionUID = 2773005455097994117L;
+
+    private UUID id;
+    private String message;
+}
